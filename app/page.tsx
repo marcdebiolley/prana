31c8e73bebe2880b83ddc52b4ac1d9cd36dd6226
Marco de Biolley <marcodebiolley@MacBook-Pro-de-Marco-2.local>
2026-03-16 13:20:02 +0100

    feat: add AboutFather section with SVG silhouette

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Nav from '@/components/Nav'
 import Hero from '@/components/Hero'
 import AboutPrana from '@/components/AboutPrana'
+import AboutFather from '@/components/AboutFather'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Nav />
       <Hero />
       <AboutPrana />
+      <AboutFather />
     </main>
   )
 }

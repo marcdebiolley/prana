d7abcf4ad1836f4e30408be83bb51b115a14bb9e
Marco de Biolley <marcodebiolley@MacBook-Pro-de-Marco-2.local>
2026-03-16 13:20:30 +0100

    feat: add Litter section with Border Collie fact cards

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Nav from '@/components/Nav'
 import Hero from '@/components/Hero'
 import AboutPrana from '@/components/AboutPrana'
 import AboutFather from '@/components/AboutFather'
+import Litter from '@/components/Litter'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <AboutPrana />
       <AboutFather />
+      <Litter />
     </main>
   )
 }
